e300ab8ce06b74f484b400d77ef0e780b301996f
Rafael Armanelli <rafael.armanelli@gmail.com>
2020-10-01 20:36:00 -0300

    Incluindo pratica02

---
+++ pratica02/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Rafael Armanelli de Araújo!',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
